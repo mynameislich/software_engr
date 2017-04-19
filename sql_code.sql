4f9e998ca53fc90895badf54019e2ea356d326f9
elmcgee <elinmcgee@gmail.com>
2017-04-18 22:42:20 -0500

    Update sql_code.sql
    delete unecessary stuff

--- sql_code.sql
+++ sql_code.sql
@@ -1,9 +1,9 @@
 CREATE TABLE user (email VARCHAR(50), password VARCHAR(100), address VARCHAR(500), zipcode VARCHAR(10), firstname VARCHAR(20), lastname VARCHAR(20), loggedin int, valid_id int, age int, work_id int, phone VARCHAR(15), gender VARCHAR(10), confirm int);
 
-//CREATE TABLE reservation (reservation_id VARCHAR(20), user_id VARCHAR(20), flight_id VARCHAR(20), bookedTime VARCHAR(100), reservation_status VARCHAR(10), ticket_price float);
+
 CREATE TABLE reservation (reservation_id VARCHAR(20), user_id VARCHAR(20), flight_id VARCHAR(20), bookedTimeArrival date, bookedTimeDeparture date, reservation_status boolean, ticket_price float, numPassengers int, currentAirport VARCHAR(40), destinationAirport VARCHAR(40), classType VARCHAR(20));
 
-//CREATE TABLE flight (flight_id VARCHAR(20), Time VARCHAR(100), origin VARCHAR(20), destinationa VARCHAR(20), airplane_id VARCHAR(20), price float);
+
 CREATE TABLE flight(flight_id VARCHAR(20), arrivingTime date, departingTime date, origin VARCHAR(20), destination VARCHAR(20), airplane_id VARCHAR(20), price float, frequency VARCHAR(20));
 CREATE TABLE bank_acc (acc_id VARCHAR(20), acc_holder_name VARCHAR(100), price float);
 
